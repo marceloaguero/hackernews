d60c98ce84937dd1420aca496a1ac79f52527147
Marcelo Aguero <marceloaguero@gmail.com>
2023-05-04 08:36:37 -0300

    arreglar router, agregar models

--- server.go
+++ server.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/go-chi/chi"
 	"github.com/marceloaguero/hackernews/graph"
 	database "github.com/marceloaguero/hackernews/internal/pkg/db/mysql"
 )
@@ -20,7 +19,7 @@ func main() {
 		port = defaultPort
 	}
 
-	router := chi.NewRouter()
+	//router := chi.NewRouter()
 
 	database.InitDB()
 	defer database.CloseDB()
@@ -28,8 +27,8 @@ func main() {
 
 	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
 
-	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	router.Handle("/query", srv)
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
