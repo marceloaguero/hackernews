0adb87f3ae9002ae4fca06155bd9519b6b687737
Marcelo Aguero <marceloaguero@gmail.com>
2023-05-04 10:56:53 -0300

    agregar middleware a server

--- server.go
+++ server.go
@@ -7,7 +7,9 @@ import (
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/go-chi/chi"
 	"github.com/marceloaguero/hackernews/graph"
+	"github.com/marceloaguero/hackernews/internal/auth"
 	database "github.com/marceloaguero/hackernews/internal/pkg/db/mysql"
 )
 
@@ -19,7 +21,9 @@ func main() {
 		port = defaultPort
 	}
 
-	//router := chi.NewRouter()
+	router := chi.NewRouter()
+
+	router.Use(auth.Middleware())
 
 	database.InitDB()
 	defer database.CloseDB()
@@ -27,8 +31,8 @@ func main() {
 
 	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
